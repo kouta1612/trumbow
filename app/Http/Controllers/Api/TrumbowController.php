3463acef355e7a1b9eac35bff36745c9c7e601da
kouta1612 <kouta1612world69@gmail.com>
2019-12-16 00:03:58 +0900

    resize image

--- app/Http/Controllers/Api/TrumbowController.php
+++ app/Http/Controllers/Api/TrumbowController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class TrumbowController extends Controller
 {
     public function upload()
     {
-        Storage::put('trumbow', request()->image);
+        $image = Image::make(request()->file('image'))->resize(300, null, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+        $name = request()->file('image')->hashName();
+        Storage::put("trumbow/{$name}", (string) $image->encode());
         return [
             'success' => true
         ];
